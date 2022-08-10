691bc42b8120dfa4bf52f1bf16e1c2f4e1a2b627
Quincy Miller <quincymiller6589@gmail.com>
2022-08-10 15:56:24 -0400

    added support for react-router

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,25 @@
 import React from "react";
 import cart from "../images/icons/cart.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const spanClasses =
+  const NavItem =
     "pl-4 pr-4 pt-1.5 hover:bg-text hover:text-darkText text-xl rounded-xl h-12";
   return (
     <div className="navbar-container pt-2 w-full h-16 flex z-50 shadow-md bg-backGround">
       <div className="navbar-left">
-        <span className={spanClasses}>Shop</span>
-        <span className={spanClasses}>Group Buys</span>
-        <span className={spanClasses}>Rent Gear</span>
-        <span className={spanClasses}>Request a Trip</span>
+        <span className={NavItem}>
+          <Link to="/shop">Shop</Link>
+        </span>
+        <span className={NavItem}>
+          <Link to="/groupbuys">Group Buys</Link>
+        </span>
+        <span className={NavItem}>
+          <Link to="/rentgear">Rent Gear</Link>
+        </span>
+        <span className={NavItem}>
+          <Link to="/requestatrip">Request a Trip</Link>
+        </span>
       </div>
       <div className="cart-container bg-text flex rounded-xl h-12 w-12">
         <img src={cart} className="flex h-9 w-9"></img>
---
+++ frontend/src/components/requestatrip/RequestATrip.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RequestATrip = () => {
+  return <div>RequestATrip</div>;
+};
+
+export default RequestATrip;
